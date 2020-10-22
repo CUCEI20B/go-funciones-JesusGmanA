96fe1efc14346a5a30f208f7d4c7a804382aa3a8
JesusGmanA <jesus.guzman8982@alumnos.udg.mx>
2020-10-21 21:29:23 -0500

    Initial commit

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,22 @@
 package main
 
-func Burbuja(s []int64)  {
-
+func exchange(x, y *int64) {
+	temp := *x
+	*x = *y
+	*y = temp
 }
 
-func main()  {
-	
+func Burbuja(s []int64) {
+	length := len(s)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-1-i; j++ {
+			if s[j] > s[j+1] {
+				exchange(&s[j], &s[j+1])
+			}
+		}
+	}
+}
+
+func main() {
+
 }
